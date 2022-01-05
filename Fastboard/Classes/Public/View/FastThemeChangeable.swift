112fe5003191114eff2e97ea5afd158182d34194
xuyunshi <405029644@qq.com>
2022-01-05 18:27:32 +0800

    Update asset

---
+++ Fastboard/Classes/Public/View/FastThemeChangeable.swift
@@ -0,0 +1,12 @@
+//
+//  FastThemeChangable.swift
+//  Fastboard
+//
+//  Created by xuyunshi on 2021/12/31.
+//
+
+import Foundation
+
+public protocol FastThemeChangeable {
+    func rebuildStyleForBeforeOS12()
+}
