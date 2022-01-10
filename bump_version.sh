7ec786809a9f0da936c8e6c11a681d7621f902d2
xuyunshi <405029644@qq.com>
2022-01-10 15:45:18 +0800

    Update bump_version.sh

--- bump_version.sh
+++ bump_version.sh
@@ -1,3 +1,34 @@
+len() {
+  echo ${#1}
+}
+
 NEWVERSION=$1
-echo 'bump version to' $NEWVERSION
+VERSIONLENGTH=$(len $NEWVERSION)
+OLDTAG=$(git describe --abbrev=0)
+echo 'input version is' $NEWVERSION
+echo 'old Version is' $OLDTAG
+if [ '0' = $VERSIONLENGTH ]; then
+echo 'empty version, please try again'
+exit 0
+elif [ $OLDTAG = $NEWVERSION ]; then
+echo 'version exist, please try again'
+exit 0
+else echo 'version enable'
+fi
+
+echo 'star bump version to' $NEWVERSION
+sed -i '' 's/'$OLDTAG'/'$NEWVERSION'/g' Fastboard.podspec
+echo 'update version text in podspec'
+git add Fastboard.podspec
+git commit -m 'Update podspec'
+echo 'git commit'
+git tag $NEWVERSION
+echo 'add tag to git'
+git push netless
+echo 'push commit to netless'
+git push netless --tags
+echo 'push tags to netless'
+echo 'begin push to trunk'
+pod trunk push Fastboard.podspec
+echo 'successfully🌹'
 
