deecd767ec88291ae98e842ef80856c247761b61
xuyunshi <405029644@qq.com>
2022-01-13 15:18:47 +0800

    Remove create function from FastboardManager

--- Fastboard/Classes/Public/FastboardManager.swift
+++ Fastboard/Classes/Public/FastboardManager.swift
@@ -9,33 +9,7 @@ import UIKit
 import Whiteboard
 
 public class FastboardManager: NSObject {
-    /// Weak obtain all all the fastboard view create by self
-    static var weakTable: NSHashTable<FastboardView> = .init(options: .weakMemory)
-    
     /// Change this to update the whiteRoom ratio and whiteboardView ratio
     @objc
     public static var globalFastboardRatio: CGFloat = 16.0 / 9.0
-    
-    @objc
-    public class func createFastboardWithConfiguration(_ configuration: FastConfiguration) -> Fastboard {
-        func defaultOverlay() -> FastboardOverlay {
-            if UIScreen.main.traitCollection.hasCompact {
-                return CompactFastboardOverlay()
-            } else {
-                return RegularFastboardOverlay()
-            }
-        }
-        let fastboardOverlay = configuration.customOverlay ?? defaultOverlay()
-        let fastboardView = FastboardView(overlay: fastboardOverlay)
-        let fastboard = Fastboard(view: fastboardView, roomConfig: configuration.whiteRoomConfig)
-        let whiteSDK = WhiteSDK(whiteBoardView: fastboardView.whiteboardView, config: configuration.whiteSdkConfiguration, commonCallbackDelegate: fastboard.sdkDelegateProxy)
-        fastboard.whiteSDK = whiteSDK
-        
-        weakTable.add(fastboardView)
-        // Make sure the method be executed once.
-        MethodHook.shared.start()
-        // Guarantee the default theme will be applied
-        ThemeManager.shared.start()
-        return fastboard
-    }
 }
