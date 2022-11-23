3830299245f146929ca9c7a2810098c08188d833
xuyunshi <405029644@qq.com>
2022-11-23 18:03:52 +0800

    Update example

--- Example/Fastboard/ViewController.swift
+++ Example/Fastboard/ViewController.swift
@@ -87,7 +87,7 @@ class ViewController: UIViewController {
                                            roomToken: RoomInfo.ROOMTOKEN.value,
                                            region: .CN,
                                            userUID: "some-unique-id-1",
-                                           userPayload: .init(nickName: "aaa", avatar: "https://t7.baidu.com/it/u=2531125946,3055766435&fm=193&f=GIF"),
+                                           userPayload: .init(nickName: "aaa", avatar: "https://example.com/avatar.jpg"),
                                            useFPA: globalUsingFPA)
         } else {
             // Without fpa
