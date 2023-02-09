8e6cdbdc762c7099a8606564e29ff8b9e976cc00
xuyunshi <405029644@qq.com>
2023-02-09 10:50:32 +0800

    Update prefersPencilOnlyDrawing calling minimal version from 14.0

--- Fastboard/Classes/Public/FastRoom/FastRoom+Pencil.swift
+++ Fastboard/Classes/Public/FastRoom/FastRoom+Pencil.swift
@@ -19,7 +19,7 @@ extension FastRoom {
     }
     
     @objc func onDidBecomeActiveNotification() {
-        if #available(iOS 13.0, *) {
+        if #available(iOS 14.0, *) {
             if view.isPencilDrawOnly != UIPencilInteraction.prefersPencilOnlyDrawing {
                 view.isPencilDrawOnly = UIPencilInteraction.prefersPencilOnlyDrawing
             }
@@ -37,7 +37,7 @@ extension FastRoom {
     
     fileprivate func updateIfFollowSystemPencilBehavior(_ follow: Bool) {
         if #available(iOS 12.1, *) {
-            if #available(iOS 13.0, *) {
+            if #available(iOS 14.0, *) {
                 view.isPencilDrawOnly = follow ? UIPencilInteraction.prefersPencilOnlyDrawing : false
             }
             if follow {
