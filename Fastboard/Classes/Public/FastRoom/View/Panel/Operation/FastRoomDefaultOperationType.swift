ff307009a76ccc6630352871e0e88bf969f58418
xuyunshi <405029644@qq.com>
2023-02-09 14:46:50 +0800

    Fix loading init shape appliance fail

--- Fastboard/Classes/Public/FastRoom/View/Panel/Operation/FastRoomDefaultOperationType.swift
+++ Fastboard/Classes/Public/FastRoom/View/Panel/Operation/FastRoomDefaultOperationType.swift
@@ -59,7 +59,7 @@ public class FastRoomDefaultOperationIdentifier: NSObject {
     
     @objc
     public var identifier: String {
-        if let shape = shape {
+        if appliance == .ApplianceShape, let shape = shape {
             return shape.rawValue
         } else if let appliance = appliance {
             return appliance.rawValue
