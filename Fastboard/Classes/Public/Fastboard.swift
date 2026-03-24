a864b7f0fd121cdb73d1287236431cb066303234
vince-hz <zjxuyunshi@gmail.com>
2026-03-24 10:45:35 +0800

    Update version number

--- Fastboard/Classes/Public/Fastboard.swift
+++ Fastboard/Classes/Public/Fastboard.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Whiteboard
 
-let versionNumber = "1.4.4"
+let versionNumber = "1.4.5"
 
 public class Fastboard: NSObject {
     /// Change this to update the whiteRoom ratio and whiteboardView ratio
